fde36d0f2a49e5c63e61af680f3cf32e8f02312b
Azamjon <alijonovrobo@gmail.com>
2025-01-08 15:24:40 +0500

    damka kodini tayyor

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from time import process_time
+
+
 def pawn(x1, y1, x2, y2):
     return y2 - y1 == 1 and abs(x2 - x1) == 1
 
 def damka(x1, y1, x2, y2):
-    pass
+    ox = abs(x1 - x2)
+    oy = abs(y1 - y2)
+
+    return ox == oy
+
+print(damka(5, 1, 5, 2))
+
+# git add .
+# git commit -m "message"
+# git push origin main/master
